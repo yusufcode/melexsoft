74b705959d58e23c8b444cf9c612673189a01c1d
Yusuf Code <yusuf1code@gmail.com>
2021-08-19 18:10:43 +0300

    services and process pages created

--- routes/indexGetRoutes.js
+++ routes/indexGetRoutes.js
@@ -2,11 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index', {activePage: 'home', navbarClass: 'navbar menu-absolute menu-center', lang: req.lang})
+    res.render('index', {activePage: req.lang.navbar_home, navbarClass: 'navbar menu-absolute menu-center', lang: req.lang})
+})
+
+router.get('/services', (req, res) => {
+    res.render('services', {activePage: req.lang.navbar_services, navbarClass: 'navbar menu-center', lang: req.lang})
+})
+
+router.get('/process', (req, res) => {
+    res.render('process', {activePage: req.lang.navbar_process, navbarClass: 'navbar menu-center', lang: req.lang})
 })
 
 router.get('/case-studies', (req, res) => {
-    res.render('case-studies', {activePage: 'case-studies', navbarClass: 'navbar menu-center', lang: req.lang})
+    res.render('case-studies', {activePage: req.lang.navbar_case_studies, navbarClass: 'navbar menu-center', lang: req.lang})
 })
 
 module.exports = router
